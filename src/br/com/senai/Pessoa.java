3eb4d337b67090857ec57b4a0c7a80aaae12cf7e
MarlonMader <mader9003@gmail.com>
2021-05-24 16:10:49 -0300

    Classe removida

---
+++ src/br/com/senai/Pessoa.java
@@ -0,0 +1,36 @@
+package br.com.senai;
+
+public class Pessoa {
+	
+	
+	//METODO PUBLIC STRING NOME;
+	//PROTECTED
+	//PRIVATE 
+	
+	//ATRIBUTOS
+	private String nome;
+	private int anoDeNascimento;
+	private String sexo;
+	
+	//METODOS
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getanoDeNascimento() {
+		return anoDeNascimento;
+		}
+	public void setAnoDeNascimento(int anoDeNascimento) {
+		this.anoDeNascimento = anoDeNascimento;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}		
+}
+	
+
